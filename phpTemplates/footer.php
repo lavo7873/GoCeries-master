6e1c38d42a065c51c2146d9f0cce2cb40fce386c
lavo7873 <linhuang1101@gmail.com>
2023-03-07 14:54:53 -0800

    update

---
+++ phpTemplates/footer.php
@@ -0,0 +1,47 @@
+<!-- FOOTER -->
+<div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Customer Service</h3>
+                    <p>Call (408) 765-4321</p>
+                </div>
+
+                <div class="footer-col-2">
+                    <img src="images/logo_transparent.png">
+                    <p>Lazy? No Problem. Your laziness is matched with our speed.
+                        </p>
+                </div>
+
+                <div class="footer-col-3">
+                    <h3>Store Hours</h3>
+                    <ul>
+                        <li>Mon - Fri: 8am - 10pm</li>
+                        <li>Sat - Sun: 10pm - 9pm</li>
+                    </ul>
+                </div>
+
+                <div class="footer-col-4">
+                    <h3>Location</h3>
+                    <ul>
+                        <li>777 The Alameda</li>
+                        <li>San Jose, CA 95126</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class = "copyright">Copyright 2020 Go!Ceries </p>
+        </div>
+    </div>
+
+
+    <!-- SELF JS SCRIPT -->
+    <script src="./index.js"></script>
+
+    <!-- BOOSTRAP JS SCRIPTS  -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
