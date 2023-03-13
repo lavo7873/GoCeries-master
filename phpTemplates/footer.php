dc1d4df69eeff2521f84b4f1024bce8f2bf67bb6
lavo7873 <linhuang1101@gmail.com>
2023-03-13 16:45:05 -0700

    update

---
+++ phpTemplates/footer.php
@@ -0,0 +1,47 @@
+<!-- FOOTER -->
+<div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Customer Service</h3>
+                    <p>Call (408) 765-4321</p>
+                </div>
+
+                <div class="footer-col-2">
+                    <img src="images/logo_transparent.png">
+                    <p>Lazy? No Problem. Your laziness is matched with our speed.
+                        </p>
+                </div>
+
+                <div class="footer-col-3">
+                    <h3>Store Hours</h3>
+                    <ul>
+                        <li>Mon - Fri: 8am - 10pm</li>
+                        <li>Sat - Sun: 10pm - 9pm</li>
+                    </ul>
+                </div>
+
+                <div class="footer-col-4">
+                    <h3>Location</h3>
+                    <ul>
+                        <li>777 The Alameda</li>
+                        <li>San Jose, CA 95126</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class = "copyright">Copyright 2020 Go!Ceries </p>
+        </div>
+    </div>
+
+
+    <!-- SELF JS SCRIPT -->
+    <script src="./index.js"></script>
+
+    <!-- BOOSTRAP JS SCRIPTS  -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ phpTemplates/mainPageHeader.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+include "db_conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Goceries | Welcome!</title>
+
+    <!-- Add boostrap CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- OWN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    
+
+</head>
+
+<body>
+
+    <!-- HEADER -->
+    <div class="header">
+        <div class = header-nav>
+            <div class = "container">
+                <div class = "navbar">
+                    <div class = "logo">
+                        <h1>GOCERIES</h1>
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li><a href = "mainPage.php">Home</a></li>
+                            <li><a href = "category-allProducts.php">Products</a></li>
+                            <li><a href = "about.php">About</a></li>
+                            <?php if (isset($_SESSION['user_id'])&& isset($_SESSION['user_loginname'])){ ?>
+
+
+                            <li><a href = "account.php">Hello, <?php echo $_SESSION['user_name']; ?></a></li>
+                            <li><a href = "logout.php">Logout</a></li>
+                                  <?php } else { ?>
+
+                                  <li><a href = "account.php">Account</a></li>
+                                  <?php } ?>
+                            <li><a href = "cart.php"><i class="fa fa-shopping-cart cart-icon" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <div class = "row">
+            <div class ="column">
+
+                <img src="images/logo_transparent.png">
+                <hr>
+                <h2>WE'RE HERE TO HELP JUSTIFY YOUR LAZINESS. <br><br>
+                    GETTING YOUR GROCERIES IS JUST A FEW CLICKS AWAY!</h2>
+                <hr>
+                    <p>
+                    How are we different? We offer the best price and delivery services.
+                    </p>
+                <p>What are you waiting for? Join us now. <br>
+                  <?php if (isset($_SESSION['user_id'])&& isset($_SESSION['user_loginname'])){ ?>
+                    
+                    <h1>Hello, <?php echo $_SESSION['user_name']; ?></h1>
+                  <a href="logout.php">Logout</a>
+                      <?php } else { ?>
+
+                        <a href = "loginregister.php" class = "btn">Sign Up &#10026;</a>
+                        <a href = "loginregister.php" class = "btn">Log in &#10026;</a>
+                      <?php } ?>
+                </p>
+            </div>
+
+        </div>
+    </div>
